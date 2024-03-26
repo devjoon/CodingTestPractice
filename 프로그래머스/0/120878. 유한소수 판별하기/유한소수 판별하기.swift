3c9765478f3520a03e7778e54f12a629d2c50c66
Morgan <101351216+devjoon@users.noreply.github.com>
2024-03-26 11:22:48 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.01 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.swift
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.swift
@@ -1,8 +1,8 @@
 import Foundation
 
 func solution(_ a:Int, _ b:Int) -> Int {
-    var gcd = GCD(a,b)
-    var b = b/gcd
+    var a = a
+    var b = b
     
     while b % 2 == 0 {
         b /= 2
@@ -12,26 +12,6 @@ func solution(_ a:Int, _ b:Int) -> Int {
         b /= 5
     }
     
-    return b == 1 ? 1 : 2
+    return a % b == 0 ? 1 : 2
     
-}
-
-func GCD(_ a:Int, _ b:Int) -> Int {
-    var a = a
-    var b = b
-    var max = [a,b].max()!
-    var min = 2
-    var answer = 1
-    
-    while min <= max {
-        if a % min == 0 && b % min == 0 {
-            answer *= min
-            a /= min
-            b /= min
-        } else {
-            min += 1
-        }
-    }
-    
-    return answer
 }
